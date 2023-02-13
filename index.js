649d8a2c597e39ae32e4b5f014554ac93908cbbc
Rebecca Lehew <lehew316@gmail.com>
2023-02-12 21:30:10 -0600

    App Qs added to index.js & other minor updates

---
+++ index.js
@@ -0,0 +1,60 @@
+// Packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// App questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Title of project?',
+    },
+    {
+        type: 'input',
+        name: 'project description',
+        message: 'Description of project?',
+    },
+    {
+        type: 'input',
+        name: 'installation instructions',
+        message: 'Installation instructions?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Usage guidelines?',
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'How to contribute?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'For questions(email)?',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'For questions(GitHub)?',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Which license?',
+        choices: ['MIT', 'GPLv3', 'Apache', 'Unlicense'],
+        filter(val) {
+            return val.toLowerCase();
+        }
+    }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
