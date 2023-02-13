4898a9189b6750826cbf22a41b3744322f4fc042
Rebecca Lehew <lehew316@gmail.com>
2023-02-13 14:13:21 -0600

    Added function to generate markdown. It works!

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,7 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  
+
 }
 
 // TODO: Create a function that returns the license link
@@ -14,8 +14,35 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+# ${data.title}
 
+## Table of Contents
+- [Project Description](#Description)
+- [Usage](#Usage)
+- [Contribution](#Contribution)
+- [Installation](#Installation)
+- [Questions](#Questions)
+- [License](#License)
+
+## Description
+${data.description}
+
+## Usage
+${data.usage}
+
+## Installation
+${data.installation}
+
+## Contribution
+${data.contribution}
+
+## Questions
+${data.email}
+${data.github}
+
+## License
+${data.license}
 `;
 }
 
