3214df771002e258ce76cf7697b707c9671574d5
Rebecca Lehew <lehew316@gmail.com>
2023-02-15 22:57:51 -0600

    Added license link and license section functions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,13 +12,22 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-
+  const licenseLink = {
+    mit: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
+    gplv3: '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)',
+    apache: '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+  };
+  return licenseLink[license];
 };
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  
+  if(license) {
+    return 'Licensed under the ${this.renderlicenseLink(license)} license.'
+  } else {
+    return ''
+  }
 };
 
 // TODO: Create a function to generate markdown for README
@@ -53,7 +62,7 @@ ${data.email}
 ${data.github}
 
 ## License
-${data.license}
+${this.renderLicenseSection(data.license)}
 `;
 }
 
