53e6a46e8fc74ca0c7c64f5d2e560350407ff1dc
Rebecca Lehew <lehew316@gmail.com>
2023-02-15 22:48:16 -0600

    Added render license function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js')
 
 // App questions for user input
 const questions = [
@@ -43,7 +44,7 @@ const questions = [
         type: 'list',
         name: 'license',
         message: 'Which license?',
-        choices: ['MIT', 'GPLv3', 'Apache', 'Unlicense'],
+        choices: ['MIT', 'GPLv3', 'Apache'],
         // Why do you have to return to lowercase?
         filter(val) {
             return val.toLowerCase();
@@ -53,17 +54,17 @@ const questions = [
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile('README.md', mark, function(err) {
+    fs.writeFile('README.md', questions, (err) => {
         if(err){
             console.log('Could not save this file!');
         } else {
             console.log('Success! New README file created in the current directory!')
         }
     })
-}
+};
 
 // TODO: Create a function to initialize app
-async function init() {
+function init() {
     return inquirer.prompt(questions)
     .then((data) => {
         console.log(data);
@@ -72,7 +73,7 @@ async function init() {
     .catch((error) => {
         console.log(error);
     })
-}
+};
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,25 +1,32 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-
-}
+  const badges = {
+    mit: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
+    gplv3: '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)',
+    apache: '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+  };
+  return badges[license];
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
 
-}
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   
-}
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
 # ${data.title}
+
+## ${this.renderLicenseBadge(data.license)}
 
 ## Table of Contents
 - [Project Description](#Description)
